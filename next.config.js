18d09703f95184a25fabbc6d40a943d663c64b6e
Anamika T S <anamikats@anamikas-mbp.in.ibm.com>
2023-12-07 16:48:04 +0530

    feat(tutorial): image loaded file added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  trailingSlash: true,
   output: 'export',
+  assetPrefix: './',
+  images: {
+    loader: 'custom',
+    loaderFile: './my-loader.ts',
+  },
 };
 
 module.exports = nextConfig;
